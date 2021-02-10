5d42bfb66b493f5661d86e8d9babf29081689238
Peter Rakyta <rakytap@caesar.elte.hu>
2021-02-10 10:01:00 +0100

    Further test cases added to matrix product calculations.

--- performance_tests/test_matrix.py
+++ performance_tests/test_matrix.py
@@ -8,7 +8,7 @@ class Test_Dot:
     """Call to test cases for multiplication of two matrices on the C++ side"""
 
 
-    matrix_size = 20
+    matrix_size = 400
     iteration_num = 100
 
 
@@ -68,6 +68,58 @@ class Test_Dot:
 
     def test_dot2(self):
         r"""
+        This method tests the functionality of calculating the dot product A * B^* of two matrices and compares it to numpy. 
+        It is called from a test file ../test_matrix.py runned by pytest
+
+        """
+
+        print(' ')
+        print('*******************************************')
+        print('Calculating matrix product A * B^*')
+
+
+        # creating a numpy matrices
+        A_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        B_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        A_numpy[0,1] = 0.2
+        B_numpy[0,1] = 0.2
+
+        time_python = 10
+        time_Cpp = 10
+
+        # calculations on the C++ side
+        for idx in range(self.iteration_num):
+            start = time.time()
+            C = dot.dot2( A_numpy, B_numpy)
+            time_loc = time.time() - start      
+
+            if time_Cpp > time_loc:
+                time_Cpp = time_loc
+
+        for idx in range(self.iteration_num):
+
+            # calculate the matrix product by numpy
+            start = time.time()
+            C_numpy = np.dot( A_numpy, B_numpy.conjugate() )
+            time_loc = time.time() - start
+
+            if time_python > time_loc:
+                time_python = time_loc
+      
+
+        # now calculate the difference between C++ and numpy
+        diff = np.linalg.norm(C_numpy - C)
+
+        print( 'Difference between numpy and C++ result: ' + str(diff))
+        print('Time elapsed with python: ' + str(time_python))
+        print('Time elapsed with C++: ' + str(time_Cpp))
+        print( "speedup: " + str(time_python/time_Cpp) )
+
+        assert diff < 1e-13
+
+
+    def test_dot3(self):
+        r"""
         This method tests the functionality of calculating the dot product A^T * B of two matrices and compares it to numpy. 
         It is called from a test file ../test_matrix.py runned by pytest
 
@@ -89,10 +141,8 @@ class Test_Dot:
 
         # calculations on the C++ side
         for idx in range(self.iteration_num):
-            A = A_numpy.copy()
-
             start = time.time()
-            C = dot.dot( A.transpose(), B_numpy)
+            C = dot.dot3( A_numpy, B_numpy)
             time_loc = time.time() - start      
 
             if time_Cpp > time_loc:
@@ -122,33 +172,166 @@ class Test_Dot:
 
 
 
-    def ctest_dote(self):
+    def test_dot4(self):
         r"""
-        This method test the dot product of two matrices. 
-        The call is forwarded to a cython class to test some functionalities of a C++ class. 
+        This method tests the functionality of calculating the dot product A^+ * B of two matrices and compares it to numpy. 
+        It is called from a test file ../test_matrix.py runned by pytest
 
         """
 
-        # creating an instance of wrapping class to call the test function by pytest
-        wrapper_for_matrix_instance = wrapper_for_test_matrix( matrix_size = 16 )
+        print(' ')
+        print('*******************************************')
+        print('Calculating matrix product A^+ * B')
 
-        # call the test function A*B
-        wrapper_for_matrix_instance.dot_test_function()
 
-        # call the test function A * B^*
-        wrapper_for_matrix_instance.dot_test_function5()
+        # creating a numpy matrices
+        A_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        B_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        A_numpy[0,1] = 0.2
+        B_numpy[0,1] = 0.2
 
-        # call the test function A^T * B
-        wrapper_for_matrix_instance.dot_test_function2()
+        time_python = 10
+        time_Cpp = 10
 
-        # call the test function A^+ * B
-        wrapper_for_matrix_instance.dot_test_function3()
+        # calculations on the C++ side
+        for idx in range(self.iteration_num):
+            start = time.time()
+            C = dot.dot4( A_numpy, B_numpy)
+            time_loc = time.time() - start      
 
-        # call the test function A^* * B
-        wrapper_for_matrix_instance.dot_test_function4()
+            if time_Cpp > time_loc:
+                time_Cpp = time_loc
 
-        # call the test function A * B^T
-        wrapper_for_matrix_instance.dot_test_function6()
+        for idx in range(self.iteration_num):
+
+            # calculate the matrix product by numpy
+            start = time.time()
+            C_numpy = np.dot( A_numpy.transpose().conjugate(), B_numpy )
+            time_loc = time.time() - start
+
+            if time_python > time_loc:
+                time_python = time_loc
+      
+
+        # now calculate the difference between C++ and numpy
+        diff = np.linalg.norm(C_numpy - C)
+
+        print( 'Difference between numpy and C++ result: ' + str(diff))
+        print('Time elapsed with python: ' + str(time_python))
+        print('Time elapsed with C++: ' + str(time_Cpp))
+        print( "speedup: " + str(time_python/time_Cpp) )
+
+        assert diff < 1e-13
+
+
+
+
+
+
+    def test_dot5(self):
+        r"""
+        This method tests the functionality of calculating the dot product A^* * B of two matrices and compares it to numpy. 
+        It is called from a test file ../test_matrix.py runned by pytest
+
+        """
+
+        print(' ')
+        print('*******************************************')
+        print('Calculating matrix product A^* * B')
+
+
+        # creating a numpy matrices
+        A_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        B_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        A_numpy[0,1] = 0.2
+        B_numpy[0,1] = 0.2
+
+        time_python = 10
+        time_Cpp = 10
+
+        # calculations on the C++ side
+        for idx in range(self.iteration_num):
+            start = time.time()
+            C = dot.dot5( A_numpy, B_numpy)
+            time_loc = time.time() - start      
+
+            if time_Cpp > time_loc:
+                time_Cpp = time_loc
+
+        for idx in range(self.iteration_num):
+
+            # calculate the matrix product by numpy
+            start = time.time()
+            C_numpy = np.dot( A_numpy.conjugate(), B_numpy )
+            time_loc = time.time() - start
+
+            if time_python > time_loc:
+                time_python = time_loc
+      
+
+        # now calculate the difference between C++ and numpy
+        diff = np.linalg.norm(C_numpy - C)
+
+        print( 'Difference between numpy and C++ result: ' + str(diff))
+        print('Time elapsed with python: ' + str(time_python))
+        print('Time elapsed with C++: ' + str(time_Cpp))
+        print( "speedup: " + str(time_python/time_Cpp) )
+
+        assert diff < 1e-13
+
+
+
+
+    def test_dot6(self):
+        r"""
+        This method tests the functionality of calculating the dot product A * B^T of two matrices and compares it to numpy. 
+        It is called from a test file ../test_matrix.py runned by pytest
+
+        """
+
+        print(' ')
+        print('*******************************************')
+        print('Calculating matrix product A * B^T')
+
+
+        # creating a numpy matrices
+        A_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        B_numpy = np.ascontiguousarray(unitary_group.rvs(self.matrix_size))
+        A_numpy[0,1] = 0.2
+        B_numpy[0,1] = 0.2
+
+        time_python = 10
+        time_Cpp = 10
+
+        # calculations on the C++ side
+        for idx in range(self.iteration_num):
+            start = time.time()
+            C = dot.dot6( A_numpy, B_numpy)
+            time_loc = time.time() - start      
+
+            if time_Cpp > time_loc:
+                time_Cpp = time_loc
+
+        for idx in range(self.iteration_num):
+
+            # calculate the matrix product by numpy
+            start = time.time()
+            C_numpy = np.dot( A_numpy, B_numpy.transpose() )
+            time_loc = time.time() - start
+
+            if time_python > time_loc:
+                time_python = time_loc
+      
+
+        # now calculate the difference between C++ and numpy
+        diff = np.linalg.norm(C_numpy - C)
+
+        print( 'Difference between numpy and C++ result: ' + str(diff))
+        print('Time elapsed with python: ' + str(time_python))
+        print('Time elapsed with C++: ' + str(time_Cpp))
+        print( "speedup: " + str(time_python/time_Cpp) )
+
+        assert diff < 1e-13
 
 
 
--- piquasso/common/dot_wrapper.cpp
+++ piquasso/common/dot_wrapper.cpp
@@ -7,94 +7,8 @@
 #include "structmember.h"
 #include "dot.h"
 #include "tbb/scalable_allocator.h"
+#include "numpy_interface.h"
 
-
-void capsule_cleanup(PyObject* capsule) {
-
-    void *memory = PyCapsule_GetPointer(capsule, NULL);
-    // I'm going to assume your memory needs to be freed with free().
-    // If it needs different cleanup, perform whatever that cleanup is
-    // instead of calling free().
-    scalable_aligned_free(memory);
-
-
-}
-
-
-PyObject* array_from_ptr(void * ptr, int dim, npy_intp* shape, int np_type) {
-
-        // create numpy array
-        PyObject* arr = PyArray_SimpleNewFromData(dim, shape, np_type, ptr);
-
-        // set memory keeper for the numpy array
-        PyObject *capsule = PyCapsule_New(ptr, NULL, capsule_cleanup);
-        PyArray_SetBaseObject((PyArrayObject *) arr, capsule);
-        
-        return arr;
-
-}
-
-
-
-/**
-r"""
-        Call to make a numpy array from an external pic::matrix class.
-
-        Args:
-        mtx (matrix&): a matrix instance
-*/
-PyObject* matrix_to_numpy( pic::matrix &mtx ) {
- 
-     
- 
-        npy_intp shape[2];
-        shape[0] = (npy_intp) mtx.rows;
-        shape[1] = (npy_intp) mtx.cols;
-
-        pic::Complex16* data = mtx.get_data();
-        return array_from_ptr( (void*) data, 2, shape, NPY_COMPLEX128);
-
-
-}
-
-
-
-
-/**
-@brief Call to create a PIC matrix representation of a numpy array
-*/
-pic::matrix 
-numpy2matrix(PyObject *arr) {
-
-    // test C-style contiguous memory allocation of the arrays
-    if ( !PyArray_IS_C_CONTIGUOUS(arr) ) {
-        std::cout << "array is not memory contiguous" << std::endl;
-    }
-
-    // get the pointer to the data stored in the input matrices
-    pic::Complex16* data = (pic::Complex16*)PyArray_DATA(arr);
-
-    // get the dimensions of the array self->C
-    int dim_num = PyArray_NDIM( arr );
-    npy_intp* dims = PyArray_DIMS(arr);
-
-    // create PIC version of the input matrices   
-    if (dim_num == 2) {
-        pic::matrix mtx = pic::matrix(data, dims[0], dims[1]);  
-        return mtx;
-    }
-    else if (dim_num == 1) {
-        pic::matrix mtx = pic::matrix(data, dims[0], 1);  
-        return mtx;
-    }
-    else {
-        std::cout << "numpy2matrix: Wrong matrix dimension was given" << std::endl;
-        return pic::matrix(0,0);
-    }
-
-
-
-} 
 
 
 
@@ -106,8 +20,8 @@ extern "C"
 
 
 /**
-@brief Wrapper function to call the simulate method of C++ class CGaussianState
-@param self A pointer pointing to an instance of the class GaussianState_Wrapper.
+@brief Wrapper function to calculate the matrix product A*B
+@param self A tuple of arguments
 @param args A tuple of the input arguments: ??????????????
 */
 static PyObject *
@@ -162,13 +76,353 @@ dot_wrapper_dot(PyObject *self, PyObject *args)
     Py_DECREF(B);  
 
 
-    return C;//Py_BuildValue("i", -1);;
+    return C;
 }
+
+
+
+/**
+@brief Wrapper function to calculate the matrix product A * B*
+@param self A tuple of arguments
+@param args A tuple of the input arguments: ??????????????
+*/
+static PyObject *
+dot_wrapper_dot2(PyObject *self, PyObject *args)
+{
+
+    // initiate variables for input arguments
+    PyObject *A_arg = NULL;
+    PyObject *B_arg = NULL;
+
+    // parsing input arguments
+    if (!PyArg_ParseTuple(args, "|OO",
+                                     &A_arg, &B_arg))
+        return Py_BuildValue("i", -1);
+
+  
+    // establish memory contiguous arrays for C calculations
+    PyObject* A = NULL, *B = NULL;  
+
+    if ( PyArray_IS_C_CONTIGUOUS(A_arg) ) {
+        A = A_arg;
+        Py_INCREF(A); 
+    }
+    else {
+        A = PyArray_FROM_OTF(A_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+    if ( PyArray_IS_C_CONTIGUOUS(B_arg) ) {
+        B = B_arg;
+        Py_INCREF(B); 
+    }
+    else {
+        B = PyArray_FROM_OTF(B_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+
+    // create PIC version of the input matrices
+    pic::matrix A_mtx = numpy2matrix(A);     
+    pic::matrix B_mtx = numpy2matrix(B); 
+    B_mtx.conjugate();    
+
+    // calculate the matrix product on the C++ side
+    pic::matrix C_mtx = dot(A_mtx, B_mtx);
+
+    // release C++ matrix from the ownership of the data. (Python would handle the release of the data)
+    C_mtx.set_owner( false );      
+
+    // convert C++ matrix into numpy array
+    PyObject* C = matrix_to_numpy( C_mtx );
+
+
+    Py_DECREF(A);   
+    Py_DECREF(B);  
+
+
+    return C;
+}
+
+
+
+
+
+
+/**
+@brief Wrapper function to calculate the matrix product A^T * B
+@param self A tuple of arguments
+@param args A tuple of the input arguments: ??????????????
+*/
+static PyObject *
+dot_wrapper_dot3(PyObject *self, PyObject *args)
+{
+
+    // initiate variables for input arguments
+    PyObject *A_arg = NULL;
+    PyObject *B_arg = NULL;
+
+    // parsing input arguments
+    if (!PyArg_ParseTuple(args, "|OO",
+                                     &A_arg, &B_arg))
+        return Py_BuildValue("i", -1);
+
+  
+    // establish memory contiguous arrays for C calculations
+    PyObject* A = NULL, *B = NULL;  
+
+    if ( PyArray_IS_C_CONTIGUOUS(A_arg) ) {
+        A = A_arg;
+        Py_INCREF(A); 
+    }
+    else {
+        A = PyArray_FROM_OTF(A_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+    if ( PyArray_IS_C_CONTIGUOUS(B_arg) ) {
+        B = B_arg;
+        Py_INCREF(B); 
+    }
+    else {
+        B = PyArray_FROM_OTF(B_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+
+    // create PIC version of the input matrices
+    pic::matrix A_mtx = numpy2matrix(A);     
+    pic::matrix B_mtx = numpy2matrix(B); 
+    A_mtx.transpose();    
+
+    // calculate the matrix product on the C++ side
+    pic::matrix C_mtx = dot(A_mtx, B_mtx);
+
+    // release C++ matrix from the ownership of the data. (Python would handle the release of the data)
+    C_mtx.set_owner( false );      
+
+    // convert C++ matrix into numpy array
+    PyObject* C = matrix_to_numpy( C_mtx );
+
+
+    Py_DECREF(A);   
+    Py_DECREF(B);  
+
+
+    return C;
+}
+
+
+
+
+
+/**
+@brief Wrapper function to calculate the matrix product A^+ * B
+@param self A tuple of arguments
+@param args A tuple of the input arguments: ??????????????
+*/
+static PyObject *
+dot_wrapper_dot4(PyObject *self, PyObject *args)
+{
+
+    // initiate variables for input arguments
+    PyObject *A_arg = NULL;
+    PyObject *B_arg = NULL;
+
+    // parsing input arguments
+    if (!PyArg_ParseTuple(args, "|OO",
+                                     &A_arg, &B_arg))
+        return Py_BuildValue("i", -1);
+
+  
+    // establish memory contiguous arrays for C calculations
+    PyObject* A = NULL, *B = NULL;  
+
+    if ( PyArray_IS_C_CONTIGUOUS(A_arg) ) {
+        A = A_arg;
+        Py_INCREF(A); 
+    }
+    else {
+        A = PyArray_FROM_OTF(A_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+    if ( PyArray_IS_C_CONTIGUOUS(B_arg) ) {
+        B = B_arg;
+        Py_INCREF(B); 
+    }
+    else {
+        B = PyArray_FROM_OTF(B_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+
+    // create PIC version of the input matrices
+    pic::matrix A_mtx = numpy2matrix(A);     
+    pic::matrix B_mtx = numpy2matrix(B); 
+    A_mtx.transpose();    
+    A_mtx.conjugate(); 
+
+    // calculate the matrix product on the C++ side
+    pic::matrix C_mtx = dot(A_mtx, B_mtx);
+
+    // release C++ matrix from the ownership of the data. (Python would handle the release of the data)
+    C_mtx.set_owner( false );      
+
+    // convert C++ matrix into numpy array
+    PyObject* C = matrix_to_numpy( C_mtx );
+
+
+    Py_DECREF(A);   
+    Py_DECREF(B);  
+
+
+    return C;
+}
+
+
+
+
+
+/**
+@brief Wrapper function to calculate the matrix product A^* * B
+@param self A tuple of arguments
+@param args A tuple of the input arguments: ??????????????
+*/
+static PyObject *
+dot_wrapper_dot5(PyObject *self, PyObject *args)
+{
+
+    // initiate variables for input arguments
+    PyObject *A_arg = NULL;
+    PyObject *B_arg = NULL;
+
+    // parsing input arguments
+    if (!PyArg_ParseTuple(args, "|OO",
+                                     &A_arg, &B_arg))
+        return Py_BuildValue("i", -1);
+
+  
+    // establish memory contiguous arrays for C calculations
+    PyObject* A = NULL, *B = NULL;  
+
+    if ( PyArray_IS_C_CONTIGUOUS(A_arg) ) {
+        A = A_arg;
+        Py_INCREF(A); 
+    }
+    else {
+        A = PyArray_FROM_OTF(A_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+    if ( PyArray_IS_C_CONTIGUOUS(B_arg) ) {
+        B = B_arg;
+        Py_INCREF(B); 
+    }
+    else {
+        B = PyArray_FROM_OTF(B_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+
+    // create PIC version of the input matrices
+    pic::matrix A_mtx = numpy2matrix(A);     
+    pic::matrix B_mtx = numpy2matrix(B); 
+    A_mtx.conjugate(); 
+
+    // calculate the matrix product on the C++ side
+    pic::matrix C_mtx = dot(A_mtx, B_mtx);
+
+    // release C++ matrix from the ownership of the data. (Python would handle the release of the data)
+    C_mtx.set_owner( false );      
+
+    // convert C++ matrix into numpy array
+    PyObject* C = matrix_to_numpy( C_mtx );
+
+
+    Py_DECREF(A);   
+    Py_DECREF(B);  
+
+
+    return C;
+}
+
+
+
+
+/**
+@brief Wrapper function to calculate the matrix product A * B^T
+@param self A tuple of arguments
+@param args A tuple of the input arguments: ??????????????
+*/
+static PyObject *
+dot_wrapper_dot6(PyObject *self, PyObject *args)
+{
+
+    // initiate variables for input arguments
+    PyObject *A_arg = NULL;
+    PyObject *B_arg = NULL;
+
+    // parsing input arguments
+    if (!PyArg_ParseTuple(args, "|OO",
+                                     &A_arg, &B_arg))
+        return Py_BuildValue("i", -1);
+
+  
+    // establish memory contiguous arrays for C calculations
+    PyObject* A = NULL, *B = NULL;  
+
+    if ( PyArray_IS_C_CONTIGUOUS(A_arg) ) {
+        A = A_arg;
+        Py_INCREF(A); 
+    }
+    else {
+        A = PyArray_FROM_OTF(A_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+    if ( PyArray_IS_C_CONTIGUOUS(B_arg) ) {
+        B = B_arg;
+        Py_INCREF(B); 
+    }
+    else {
+        B = PyArray_FROM_OTF(B_arg, NPY_COMPLEX128, NPY_ARRAY_IN_ARRAY);
+    }
+
+
+    // create PIC version of the input matrices
+    pic::matrix A_mtx = numpy2matrix(A);     
+    pic::matrix B_mtx = numpy2matrix(B); 
+    B_mtx.transpose(); 
+
+    // calculate the matrix product on the C++ side
+    pic::matrix C_mtx = dot(A_mtx, B_mtx);
+
+    // release C++ matrix from the ownership of the data. (Python would handle the release of the data)
+    C_mtx.set_owner( false );      
+
+    // convert C++ matrix into numpy array
+    PyObject* C = matrix_to_numpy( C_mtx );
+
+
+    Py_DECREF(A);   
+    Py_DECREF(B);  
+
+
+    return C;
+}
+
 
 
 static PyMethodDef dot_wrapper_Methods[] = {
     {"dot", (PyCFunction) dot_wrapper_dot, METH_VARARGS,
-     "Method to calculate the product of two matrices"
+     "Method to calculate the matorx product A*B"
+    },
+    {"dot2", (PyCFunction) dot_wrapper_dot2, METH_VARARGS,
+     "Method to calculate the matorx product A * B*"
+    },
+    {"dot3", (PyCFunction) dot_wrapper_dot3, METH_VARARGS,
+     "Method to calculate the matorx product A^T * B"
+    },
+    {"dot4", (PyCFunction) dot_wrapper_dot4, METH_VARARGS,
+     "Method to calculate the matorx product A^+ * B"
+    },
+    {"dot5", (PyCFunction) dot_wrapper_dot5, METH_VARARGS,
+     "Method to calculate the matorx product A^* * B"
+    },
+    {"dot6", (PyCFunction) dot_wrapper_dot6, METH_VARARGS,
+     "Method to calculate the matorx product A * B^T"
     },
     {NULL}  /* Sentinel */
 };
