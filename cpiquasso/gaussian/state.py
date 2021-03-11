b9fa9897ef25d49990e420cfa3a4813e590196d8
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2021-03-11 17:13:36 +0100

    update(piquasso)
    A validation of `C` and `G` has been included in
    piquasso's `GaussianState.__init__`. This has to be done in cpiquasso as
    well, because this validation error is tested in piquasso too.

--- cpiquasso/gaussian/state.py
+++ cpiquasso/gaussian/state.py
@@ -3,10 +3,21 @@
 
 import piquasso as pq
 
+from piquasso.api.errors import StatePreparationError
+from piquasso._math.linalg import is_symmetric, is_selfadjoint
+
 from .state_wrapper import GaussianState_Wrapper
 
 
 class GaussianState(GaussianState_Wrapper, pq.GaussianState):
+    def __init__(self, C, G, m):
+        if not is_selfadjoint(C):
+            raise StatePreparationError("C should be self-adjoint matrix.")
+        if not is_symmetric(G):
+            raise StatePreparationError("G should be symmetric matrix.")
+
+        super().__init__(C, G, m)
+
     def apply_passive(self, T, modes):
         self.m[modes, ] = T @ self.m[modes, ]
         super().apply_to_C_and_G(T, modes)
