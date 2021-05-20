99cb94f0cb74bc83b7d1912b6dd25ad8dcf2a874
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2021-05-20 15:27:11 +0200

    fix(performance_tests): Boson Sampling
    The `test_GeneralizedCliffordsSimulationStrategy` tests with a
    non-unitary interferometer, resulting in incorrect samples.
    
    The test were rewritten using regular piquasso instructions.
    
    A simpler test scenario has also been written.
    
    The expected sample probabilities were written in the docstrings of the
    tests.

---
+++ performance_tests/test_boson_sampling.py
@@ -0,0 +1,90 @@
+#
+# Copyright (C) 2020 by TODO - All rights reserved.
+#
+
+import time
+import pytest
+import itertools
+
+import numpy as np
+
+import piquasso as pq
+
+
+@pytest.fixture
+def print_histogram():
+    def func(samples):
+        hist = dict()
+
+        for sample in samples:
+            key = tuple(sample)
+            if key in hist.keys():
+                hist[key] += 1
+            else:
+                hist[key] = 1
+
+        for key in hist.keys():
+            print(f"{key}: {hist[key]}")
+
+    return func
+
+
+def test_two_mode_sampling(print_histogram):
+    """
+    NOTE: Expected distribution probabilities:
+    (1, 1):  0.250
+    (0, 2):  0.375
+    (2, 0):  0.375
+    """
+
+    shots = 10000
+    for _ in itertools.repeat(None, 10):
+
+        pq.constants.seed()
+
+        with pq.Program() as program:
+            pq.Q() | pq.SamplingState(1, 1)
+
+            pq.Q(0, 1) | pq.Beamsplitter(np.pi / 3)
+
+            pq.Q() | pq.Sampling(shots=shots)
+
+        t0 = time.time()
+
+        results = program.execute()
+
+        print("C++ time elapsed:", time.time() - t0, "s")
+
+        print_histogram(results[0].samples)
+
+
+def test_complex_sampling(print_histogram):
+    """
+    NOTE: Expected distribution probabilities:
+    (0, 2, 0, 1, 1): 0.1875
+    (0, 2, 1, 0, 1): 0.1875
+    (1, 1, 0, 1, 1): 0.1250
+    (1, 1, 1, 0, 1): 0.1250
+    (2, 0, 0, 1, 1): 0.1875
+    (2, 0, 1, 0, 1): 0.1875
+    """
+
+    for _ in itertools.repeat(None, 10):
+        shots = 10000
+
+        with pq.Program() as program:
+            pq.Q() | pq.SamplingState(1, 1, 1, 0, 1)
+
+            pq.Q(0, 1) | pq.Beamsplitter(np.pi / 3)
+            pq.Q(2)    | pq.Fourier()
+            pq.Q(2, 3) | pq.Beamsplitter(np.pi / 4)
+
+            pq.Q() | pq.Sampling(shots=shots)
+
+        t0 = time.time()
+
+        results = program.execute()
+
+        print("C++ time elapsed:", time.time() - t0, "s")
+
+        print_histogram(results[0].samples)
