d6b5933c4d4454356ca026c71a8a60a1f8e83745
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2021-08-05 19:05:46 +0200

    benchmark: MIS comparison

---
+++ threshold_comparison.py
@@ -0,0 +1,276 @@
+#
+# Copyright 2021 Budapest Quantum Computing Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Run me with
+.. code-block:: bash
+    python boostbenchmarks/gbs_comparison_nondisplaced.py
+"""
+
+import os
+import sys
+import json
+import time
+import scipy
+import itertools
+import subprocess
+import numpy as np
+
+from scipy.optimize import root_scalar
+
+import matplotlib.pyplot as plt
+
+import piquasso as pq
+
+from piquassoboost import patch
+
+
+patch()
+
+sys.path.append(".")
+sys.path.append("./gbs")
+
+from thewalrus.quantum import mean_clicks
+from gbs.MIS_click import ClickMIS
+
+
+
+SHOTS = 10
+ITERATIONS = 5
+
+#adaptively set limit for SF and PQ if the runtime exceeds these limits
+# maximal runtime in sec for 1 iteration
+TIME_LIMIT_ITERATION = 1000
+# maximum time in sec for all iterations
+OVERALL_TIME_LIMIT = 1000
+
+# manually limiting the SF and PQ mode number
+LIMIT_SF = 100
+LIMIT_PQ = 100
+
+NUMBER_OF_MODES = list( range(2, 20) )  # NOTE: The range must start with 2 at least.
+MEAN_PHOTON_NUMBER = 0.5
+
+LOGARITHMIZE = True
+
+
+
+def _run_pq_simulation(params, d) -> float:
+    with pq.Program() as pq_program:
+        pq.Q() | pq.GaussianState(d=d)
+
+        pq.Q(all) | pq.Squeezing(r=params["squeezing"])
+
+        pq.Q(all) | pq.Interferometer(params["interferometer"])
+
+        # NOTE: In SF the cutoff is 5, and couldn't be changed
+        pq.Q(all) | pq.ThresholdMeasurement()
+
+    start_time = time.time()
+    pq_program.execute(shots=SHOTS)
+    return time.time() - start_time
+
+def _run_sf_simulation(params, d) -> float:
+    with pq.Program() as pq_program:
+        pq.Q() | pq.GaussianState(d=d)
+
+        pq.Q(all) | pq.Squeezing(r=params["squeezing"])
+
+        pq.Q(all) | pq.Interferometer(params["interferometer"])
+
+    start_time = time.time()
+
+    pq_program.execute()
+
+    cov = pq_program.state.xp_cov / 2
+    N = int(np.round(mean_clicks(cov)))
+    mis = ClickMIS(cov, N)
+    mis.run_chain(120)
+
+    return time.time() - start_time
+
+def _get_scaling(singular_values, mean_photon_number):
+    def mean_photon_number_equation(scaling):
+        return sum(
+            (scaling * singular_value) ** 2 / (1 - (scaling * singular_value) ** 2)
+            for singular_value
+            in singular_values
+        ) / len(singular_values) - mean_photon_number
+
+    def mean_photon_number_gradient(scaling):
+        return (
+            (2.0 / scaling)
+            * np.sum(
+                (
+                    singular_values * scaling
+                    / (1 - (singular_values * scaling) ** 2)
+                ) ** 2
+            )
+        )
+
+    lower_bound = 0.0
+
+    tolerance = 1e-10  # Needed to avoid zero division.
+
+    upper_bound = 1.0 / (max(singular_values) + tolerance)
+
+    result = root_scalar(
+        mean_photon_number_equation,
+        fprime=mean_photon_number_gradient,
+        x0=(lower_bound - upper_bound) / 2.0,
+        bracket=(lower_bound, upper_bound),
+    )
+
+    if not result.converged:
+        raise RuntimeError("No scaling found.")
+
+    return result.root
+
+def _json_dump(results):
+    file_prefix = f"{os.path.basename(__file__)}_{int(time.time())}"
+    json_filename = f"{file_prefix}.json"
+
+    with open(json_filename, "w") as f:
+        json.dump(results, f)
+
+def _scale_squeezing(squeezing, mean_photon_number):
+    scaling = _get_scaling(
+        singular_values=squeezing,
+        mean_photon_number=mean_photon_number
+    )
+
+    return scaling * squeezing
+
+def _generate_parameters(d, mean_photon_number):
+    squeezing = np.random.uniform(low=0.0, high=1.0, size=d)
+
+    scaled_squeezing = _scale_squeezing(squeezing, mean_photon_number)
+
+    return {
+        "squeezing": scaled_squeezing,
+        "interferometer": scipy.stats.unitary_group.rvs(d)
+    }
+
+
+def _benchmark():
+    results = {
+        "shots": SHOTS,
+        "iterations": ITERATIONS,
+        "mean_photon_number": MEAN_PHOTON_NUMBER,
+        "number_of_modes": list(NUMBER_OF_MODES),
+        "limit_of_PQ": LIMIT_PQ,
+        "limit_of_SF": LIMIT_SF,
+        "time_limit": TIME_LIMIT_ITERATION,
+        "overall_time_limit": OVERALL_TIME_LIMIT,
+        "pq_averages": [],
+        "sf_averages": [],
+        "omp_num_threads": os.getenv('OMP_NUM_THREADS')
+    }
+
+    for d in NUMBER_OF_MODES:
+        pq_times = []
+        sf_times = []
+        for _ in itertools.repeat(None, ITERATIONS):
+            params = _generate_parameters(d, MEAN_PHOTON_NUMBER)
+
+            if d <= results['limit_of_PQ']:
+                pq_times.append(_run_pq_simulation(params, d))
+            if d <= results['limit_of_SF']:
+                sf_times.append(_run_sf_simulation(params, d))
+
+        if d <= results['limit_of_PQ']:
+            sum_pq_times = sum(pq_times)
+            pq_average = sum_pq_times / len(pq_times)
+            results["pq_averages"].append(pq_average)
+            if max(pq_times) > results['time_limit'] or sum_pq_times > results['overall_time_limit']:
+                results['limit_of_PQ'] = d
+
+        if d <= results['limit_of_SF']:
+            sum_sf_times = sum(sf_times)
+            sf_average = sum_sf_times / len(sf_times)
+            results["sf_averages"].append(sf_average)
+            if max(sf_times) > results['time_limit'] or sum_sf_times > results['overall_time_limit']:
+                results['limit_of_SF'] = d
+        
+        if d % 5 == 0 and d < max(results['limit_of_SF'], results['limit_of_PQ']):
+            _json_dump(results)
+
+        print("simulation run with ", d, " modes")
+
+
+    print("\nSIMULATION RESULTS:\n")
+
+    print("Mean photon number:", MEAN_PHOTON_NUMBER)
+    print("d:", list(NUMBER_OF_MODES))
+    print("piquasso   (s):", results["pq_averages"])
+    print("strawberry (s):", results["sf_averages"])
+
+    # PLOTTING
+
+    x_axis_sf = list( filter(lambda i: i <= results['limit_of_SF'], NUMBER_OF_MODES) )
+    x_axis_pq = list( filter(lambda i: i <= results['limit_of_PQ'], NUMBER_OF_MODES) )
+
+    x_axis = list(set(x_axis_pq) | set(x_axis_sf))
+    results['number_of_modes'] = x_axis
+
+    plt.rcParams.update({'font.size': 16})
+    plt.rcParams['lines.markersize'] *= 2
+
+    plt.plot(
+        x_axis_pq,
+        results["pq_averages"],
+        'bx',
+        x_axis_sf,
+        results["sf_averages"],
+        'rx',
+    )
+
+    plt.xlim([min(x_axis) - 0.5, max(x_axis) + 0.5])
+
+    plt.xlabel("Number of modes (d)")
+    plt.ylabel(
+        "Computation time [s]"
+        if LOGARITHMIZE
+        else "Computation time [s]"
+    )
+
+    plt.xticks(x_axis)
+
+    if not LOGARITHMIZE:
+        plt.gca().set_ylim(bottom=0)
+    else:
+        plt.yscale("log")
+
+    file_prefix = f"{os.path.basename(__file__)}_{int(time.time())}"
+
+    svg_filename = f"{file_prefix}.png"
+    json_filename = f"{file_prefix}.json"
+
+    with open(json_filename, "w") as f:
+        json.dump(results, f)
+
+    plt.savefig(svg_filename)
+
+    subprocess.call(('xdg-open', svg_filename))
+
+if __name__ == "__main__":
+    try:
+        _benchmark()
+    except KeyboardInterrupt:
+        file_prefix = f"{os.path.basename(__file__)}_{int(time.time())}"
+        json_filename = f"{file_prefix}.json"
+        with open(json_filename, "w") as f:
+            json.dump(results, f)
+
