c5f4c6994e36f213cf21919308532018ea6dc457
Agoston Kaposi <kaposiagoston@inf.elte.hu>
2021-11-09 22:33:12 +0100

    v

---
+++ piquassoboost/sampling/source/PermanentCalculator.cpp
@@ -0,0 +1,134 @@
+#include "PermanentCalculator.h"
+
+namespace pic {
+
+
+PermanentCalculator::PermanentCalculator(matrix &mtx)
+    : mtx(mtx)
+{
+
+}
+
+Complex16 PermanentCalculator::calculatePermanent(
+    PicState_int64 &inputState,
+    PicState_int64 &outputState
+){
+    sumOfPartialPermanents = ComplexM<long double>();
+
+    // row multiplicities are determined by the output state
+    PicState_int rowMultiplicities =
+        convert_PicState_int64_to_PicState_int(outputState);
+
+    initialRowMultiplicities = rowMultiplicities.copy();
+
+    // column multiplicities are determined by the input state
+    colMultiplicities =
+        convert_PicState_int64_to_PicState_int(inputState);
+
+    // create vector of values which determine whether the specific row has to be added or not
+    // 0 means not
+    // 1 means it has to be added to the first row with multiplicity rowMultiplicity[i]
+    rowSummation = PicState_int(outputState.size());
+    for (int i = 0; i < rowSummation.size(); i++){
+        rowSummation[i] = 
+            rowMultiplicities[i] % 2 == 0 ?
+                1 :
+                0;
+    }
+
+    calculatePermanentWithStartIndex(rowMultiplicities, 0);
+
+
+}
+
+
+
+void PermanentCalculator::calculatePermanentWithStartIndex(
+    PicState_int& rowMultiplicities,
+    int startIndex
+){
+    const int rows = rowMultiplicities.size();
+
+    while (startIndex < rows && rowMultiplicities[startIndex] <= 1){
+        startIndex++;
+    }
+
+    if (startIndex == rows){
+        // create matrix with the given values
+        // calculate permanent
+
+        calculatePermanentFromExplicitMatrix(
+            rowMultiplicities
+        );
+    }else{
+        // here the multiplicity is higher than 1 !
+        const int rowMultiplicity = rowMultiplicities[startIndex];
+        // even case
+        if ( 0 == rowMultiplicity % 2 ){
+            // for each number until the rowMultiplicities[i]
+
+            // create new copy of rowMultiplicities.
+            // rowMultiplicities has to be each values corresponding to rowMultiplicities[i]
+
+            for (int multiplicity = -rowMultiplicity; multiplicity < rowMultiplicity; multiplicity += 2){
+                PicState_int newRowMultiplicities = rowMultiplicities.copy();
+                rowMultiplicities[startIndex] = multiplicity;
+        
+                calculatePermanentWithStartIndex(
+                    newRowMultiplicities,
+                    startIndex + 1
+                );
+            }
+        }
+        // odd case
+        else{
+            // create other matrix with the same rows
+            // the i'th row has to be multiplied with the numbers from 1 to rowMultiplicities[i]
+            // sum up the calculated values with coefficients
+            for (int multiplicity = 1; multiplicity <= rowMultiplicity; multiplicity += 2){
+                PicState_int newRowMultiplicities = rowMultiplicities.copy();
+                rowMultiplicities[startIndex] = multiplicity;
+        
+                calculatePermanentWithStartIndex(
+                    newRowMultiplicities,
+                    startIndex + 1
+                );                
+            }
+        }
+
+    }
+}
+
+Complex16 PermanentCalculator::calculatePermanentFromExplicitMatrix(
+    PicState_int& rowMultiplicities
+){
+    std::cout << "permanent calculation" << std::endl;
+    std::cout << "rowSummation: ";
+    for (int i = 0; i < rowSummation.size(); i++){
+        std::cout << rowSummation[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "colMultiplicities: ";
+    for (int i = 0; i < colMultiplicities.size(); i++){
+        std::cout << colMultiplicities[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "rowMultiplicities: ";
+    for (int i = 0; i < rowMultiplicities.size(); i++){
+        std::cout << rowMultiplicities[i] << " ";
+    }
+    std::cout << std::endl;
+    mtx.print_matrix();
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+} // PIC
---
+++ piquassoboost/sampling/source/PermanentCalculator.h
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2021 Budapest Quantum Computing Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PERMANENT_CALCULATOR_H
+#define PERMANENT_CALCULATOR_H
+
+#include "matrix.h"
+#include "matrix32.h"
+#include "PicState.h"
+
+#ifndef CPYTHON
+#include <tbb/tbb.h>
+#endif
+
+namespace pic {
+
+class PermanentCalculator{
+    matrix& mtx;
+    //PicState_int rowMultiplicities;
+    PicState_int colMultiplicities;
+    PicState_int initialRowMultiplicities;
+
+    PicState_int rowSummation;
+    /// storage for partial permanents
+    ComplexM<long double> sumOfPartialPermanents;
+
+    PermanentCalculator(matrix& mtx);
+
+    Complex16 calculatePermanent(
+        PicState_int64& inputState,
+        PicState_int64& outputState
+    );
+
+    void PermanentCalculator::calculatePermanentWithStartIndex(
+        PicState_int& rowMultiplicities,
+        int startIndex
+    );
+
+    Complex16 calculate(matrix mtx);
+
+    Complex16 calculatePermanentFromExplicitMatrix(
+        PicState_int& rowMultiplicities
+    );
+
+};
+
+} // PIC
+
+#endif // PERMANENT_CALCULATOR_H
