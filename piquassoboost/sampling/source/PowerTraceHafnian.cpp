063a3a32fb2ab2766cec703506fe34f3f3030396
Gregory Morse <gregory.morse@live.com>
2022-08-14 23:15:34 +0200

    Update PowerTraceHafnian.cpp

--- piquassoboost/sampling/source/PowerTraceHafnian.cpp
+++ piquassoboost/sampling/source/PowerTraceHafnian.cpp
@@ -17,6 +17,7 @@
 #ifndef LONG_DOUBLE_CUTOFF
 #define LONG_DOUBLE_CUTOFF 40
 #endif // LONG_DOUBLE_CUTOFF
+#define GLYNN
 
 #include <iostream>
 #include "PowerTraceHafnian.h"
@@ -102,7 +103,11 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate() {
     int current_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &current_rank);
 
+#ifdef GLYNN
+    Complex16 hafnian = calculate(current_rank, world_size, permutation_idx_max/2);
+#else
     Complex16 hafnian = calculate(current_rank+1, world_size, permutation_idx_max);
+#endif
 
     // send the calculated partial hafnian to rank 0
     Complex16* partial_hafnians = new Complex16[world_size];
@@ -125,7 +130,11 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate() {
     unsigned long long current_rank = 0;
     unsigned long long world_size = 1;
 
+#ifdef GLYNN
+    Complex16 hafnian = calculate(current_rank, world_size, permutation_idx_max/2);
+#else
     Complex16 hafnian = calculate(current_rank+1, world_size, permutation_idx_max);
+#endif
 
     return hafnian;
 #endif
@@ -134,18 +143,10 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate() {
 
 }
 
-
-/**
-@brief Call to calculate the hafnian of a complex matrix
-@param start_idx The minimal index evaluated in the exponentially large sum (used to divide calculations between MPI processes)
-@param step_idx The index step in the exponentially large sum (used to divide calculations between MPI processes)
-@param max_idx The maximal indexe valuated in the exponentially large sum (used to divide calculations between MPI processes)
-@return Returns with the calculated hafnian
-*/
-template <class small_scalar_type, class scalar_type>
+template <>
 Complex16
-PowerTraceHafnian<small_scalar_type, scalar_type>::calculate(unsigned long long start_idx, unsigned long long step_idx, unsigned long long max_idx ) {
-    using complex_type = cplx_select_t<scalar_type>;
+PowerTraceHafnian<RationalInf, RationalInf>::calculate(unsigned long long start_idx, unsigned long long step_idx, unsigned long long max_idx ) {
+    using complex_type = cplx_select_t<RationalInf>;
     using matrix_type = mtx_select_t<complex_type>;
     if ( mtx.rows != mtx.cols) {
         std::cout << "The input matrix should be square shaped, bu matrix with " << mtx.rows << " rows and with " << mtx.cols << " columns was given" << std::endl;
@@ -194,7 +195,245 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate(unsigned long long 
 */
 
 
+#ifdef GLYNN
+        matrix_type B(dim, dim);
+        bool fact = false;
+        //also possible to the pairs of rows and pairs of columns, places them on the opposite halves, and reverses these halves 
+        for (size_t idx = 0; idx < dim; idx++) {
+            size_t row_offset = (idx ^ 1)*mtx.stride;
+            size_t swap_row_offset = idx*B.stride;
+            for (size_t jdx = 0; jdx < dim_over_2; jdx++) {
+                bool neg = (permutation_idx & (1ULL << jdx)) != 0;
+                if (idx == jdx) fact ^= neg;
+                if (neg) {
+                    ::new (&B[swap_row_offset + jdx*2]) complex_type(-mtx[ row_offset + jdx*2].real(), -mtx[ row_offset + jdx*2].imag());
+                    ::new (&B[swap_row_offset + jdx*2+1]) complex_type(-mtx[ row_offset + jdx*2+1].real(), -mtx[ row_offset + jdx*2+1].imag());                   
+                } else {
+                    ::new (&B[swap_row_offset + jdx*2]) complex_type(mtx[ row_offset + jdx*2].real(), mtx[ row_offset + jdx*2].imag());
+                    ::new (&B[swap_row_offset + jdx*2+1]) complex_type(mtx[ row_offset + jdx*2+1].real(), mtx[ row_offset + jdx*2+1].imag());
+                }
+            }
+        }
+#else
+        // get the binary representation of permutation_idx
+        // also get the number of 1's in the representation and their position as 2*i and 2*i+1 in consecutive slots of the vector bin_rep
+        std::vector<unsigned char> bin_rep;
+        std::vector<unsigned char> positions_of_ones;
+        bin_rep.reserve(dim_over_2);
+        positions_of_ones.reserve(dim);
+        for (int i = 1 << (dim_over_2-1); i > 0; i = i / 2) {
+            if (permutation_idx & i) {
+                bin_rep.push_back(1);
+                positions_of_ones.push_back((bin_rep.size()-1)*2);
+                positions_of_ones.push_back((bin_rep.size()-1)*2+1);
+            }
+            else {
+                bin_rep.push_back(0);
+            }
+        }
+        size_t number_of_ones = positions_of_ones.size();
 
+
+        // matrix B corresponds to A^(Z), i.e. to the square matrix constructed from
+        // the elements of mtx=A indexed by the rows and colums, where the binary representation of
+        // permutation_idx was 1
+        // for details see the text below Eq.(3.20) of arXiv 1805.12498
+        matrix_type B(number_of_ones, number_of_ones);
+        for (size_t idx = 0; idx < number_of_ones; idx++) {
+            size_t row_offset = (positions_of_ones[idx] ^ 1)*mtx.stride;
+            for (size_t jdx = 0; jdx < number_of_ones; jdx++) {
+                ::new (&B[idx*number_of_ones + jdx]) complex_type(mtx[ row_offset + positions_of_ones[jdx]].real(), mtx[ row_offset + positions_of_ones[jdx]].imag());
+            }
+        }
+        // fact corresponds to the (-1)^{(n/2) - |Z|} prefactor from Eq (3.24) in arXiv 1805.12498
+        bool fact = ((dim_over_2 - number_of_ones/2) % 2);
+#endif
+
+        // calculating Tr(B^j) for all j's that are 1<=j<=dim/2
+        // this is needed to calculate f_G(Z) defined in Eq. (3.17b) of arXiv 1805.12498
+        matrix_type traces(dim_over_2, 1);
+#ifndef GLYNN
+        if (number_of_ones != 0) {
+#endif
+            CalcPowerTraces<RationalInf, RationalInf>(B, dim_over_2, traces);
+#ifndef GLYNN
+        }
+        else {
+            // in case we have no 1's in the binary representation of permutation_idx we get zeros
+            // this occurs once during the calculations
+            //memset( traces.get_data(), 0.0, traces.rows*traces.cols*sizeof(complex_type));
+            std::uninitialized_fill_n(traces.get_data(), traces.rows*traces.cols, complex_type(0.0, 0.0));
+        }
+#endif
+
+        // auxiliary data arrays to evaluate the second part of Eqs (3.24) and (3.21) in arXiv 1805.12498
+        matrix_type aux0(dim_over_2 + 1, 1);
+        matrix_type aux1(dim_over_2 + 1, 1);
+        //memset( aux0.get_data(), 0.0, (dim_over_2 + 1)*sizeof(complex_type));        
+        //memset( aux1.get_data(), 0.0, (dim_over_2 + 1)*sizeof(complex_type));
+        std::uninitialized_fill_n(aux0.get_data(), dim_over_2 + 1, complex_type(0.0, 0.0));
+        std::uninitialized_fill_n(aux1.get_data(), dim_over_2 + 1, complex_type(0.0, 0.0));
+        aux0[0] = 1.0;
+        // pointers to the auxiliary data arrays
+        complex_type *p_aux0=NULL, *p_aux1=NULL;
+
+        for (size_t idx = 1; idx <= dim_over_2; idx++) {
+
+
+            complex_type factor = traces[idx - 1] / (2.0 * idx);
+
+            complex_type powfactor(1.0,0.0);
+
+
+
+            if (idx%2 == 1) {
+                p_aux0 = aux0.get_data();
+                p_aux1 = aux1.get_data();
+            }
+            else {
+                p_aux0 = aux1.get_data();
+                p_aux1 = aux0.get_data();
+            }
+
+            //memcpy(p_aux1, p_aux0, (dim_over_2+1)*sizeof(complex_type) );
+            std::copy_n(p_aux0, dim_over_2+1, p_aux1);
+
+            for (size_t jdx = 1; jdx <= (dim / (2 * idx)); jdx++) {
+                powfactor *= factor / jdx;
+
+                for (size_t kdx = idx * jdx + 1; kdx <= dim_over_2 + 1; kdx++) {
+                    p_aux1[kdx-1] += p_aux0[kdx-idx*jdx - 1]*powfactor;
+                }
+
+
+
+            }
+
+
+
+        }
+
+        if (fact) {
+            summand -= p_aux1[dim_over_2];
+//std::cout << -p_aux1[dim_over_2] << std::endl;
+        }
+        else {
+            summand += p_aux1[dim_over_2];
+//std::cout << p_aux1[dim_over_2] << std::endl;
+        }
+
+        for (size_t n = aux0.size(); n > 0; --n) aux0[n-1].~complex_type();
+        for (size_t n = aux1.size(); n > 0; --n) aux1[n-1].~complex_type();
+        for (size_t n = traces.size(); n > 0; --n) traces[n-1].~complex_type();
+        for (size_t n = B.size(); n > 0; --n) B[n-1].~complex_type();
+
+    });
+
+    // the resulting Hafnian of matrix mat
+    complex_type res(0,0);
+    summands.combine_each([&res](const complex_type& a) {
+        res += a;
+    });
+
+    //Complex16 res = summand;
+
+
+#if BLAS==0 // undefined BLAS
+    omp_set_num_threads(NumThreads);
+#elif BLAS==1 //MKL
+    MKL_Set_Num_Threads(NumThreads);
+#elif BLAS==2 //OpenBLAS
+    openblas_set_num_threads(NumThreads);
+#endif
+
+#ifdef GLYNN
+    res /= (1ULL << (dim_over_2-1));
+#endif
+    return Complex16(res.real(), res.imag() );
+}
+
+
+/**
+@brief Call to calculate the hafnian of a complex matrix
+@param start_idx The minimal index evaluated in the exponentially large sum (used to divide calculations between MPI processes)
+@param step_idx The index step in the exponentially large sum (used to divide calculations between MPI processes)
+@param max_idx The maximal indexe valuated in the exponentially large sum (used to divide calculations between MPI processes)
+@return Returns with the calculated hafnian
+*/
+template <class small_scalar_type, class scalar_type>
+Complex16
+PowerTraceHafnian<small_scalar_type, scalar_type>::calculate(unsigned long long start_idx, unsigned long long step_idx, unsigned long long max_idx ) {
+    using complex_type = cplx_select_t<scalar_type>;
+    using matrix_type = mtx_select_t<complex_type>;
+    if ( mtx.rows != mtx.cols) {
+        std::cout << "The input matrix should be square shaped, but matrix with " << mtx.rows << " rows and with " << mtx.cols << " columns was given" << std::endl;
+        std::cout << "Returning zero" << std::endl;
+        return Complex16(0,0);
+    }
+
+    if (mtx.rows == 0) {
+        // the hafnian of an empty matrix is 1 by definition
+        return Complex16(1,0);
+    }
+    else if (mtx.rows % 2 != 0) {
+        // the hafnian of odd shaped matrix is 0 by definition
+        return Complex16(0.0, 0.0);
+    }
+
+#if BLAS==0 // undefined BLAS
+    int NumThreads = omp_get_max_threads();
+    omp_set_num_threads(1);
+#elif BLAS==1 // MKL
+    int NumThreads = mkl_get_max_threads();
+    MKL_Set_Num_Threads(1);
+#elif BLAS==2 //OpenBLAS
+    int NumThreads = openblas_get_num_threads();
+    openblas_set_num_threads(1);
+#endif
+
+    size_t dim = mtx.rows;
+    size_t dim_over_2 = mtx.rows / 2;
+    //unsigned long long permutation_idx_max = power_of_2( (unsigned long long) dim_over_2);
+
+
+    // thread local storages for the partial hafnians
+    tbb::combinable<complex_type> summands{[](){return complex_type(0.0,0.0);}};
+
+    // for cycle over the permutations n/2 according to Eq (3.24) in arXiv 1805.12498
+    tbb::parallel_for( start_idx, max_idx, step_idx, [&](unsigned long long permutation_idx) {
+
+
+        complex_type &summand = summands.local();
+
+/*
+    complex_type summand(0.0,0.0);
+
+    for (unsigned long long permutation_idx = 0; permutation_idx < permutation_idx_max; permutation_idx++) {
+*/
+
+#ifdef GLYNN
+        mtx_select_t<cplx_select_t<small_scalar_type>> B(dim, dim);
+        small_scalar_type scale_factor_B = 0.0;
+        bool fact = false;
+        //also possible to the pairs of rows and pairs of columns, places them on the opposite halves, and reverses these halves 
+        for (size_t idx = 0; idx < dim; idx++) {
+            size_t row_offset = (idx ^ 1)*mtx.stride;
+            size_t swap_row_offset = idx*B.stride;
+            for (size_t jdx = 0; jdx < dim_over_2; jdx++) {
+                bool neg = (permutation_idx & (1ULL << jdx)) != 0;
+                if (idx == jdx) fact ^= neg;
+                if (neg) {
+                    ::new (&B[swap_row_offset + jdx*2]) cplx_select_t<small_scalar_type>(-mtx[ row_offset + jdx*2].real(), -mtx[ row_offset + jdx*2].imag());
+                    ::new (&B[swap_row_offset + jdx*2+1]) cplx_select_t<small_scalar_type>(-mtx[ row_offset + jdx*2+1].real(), -mtx[ row_offset + jdx*2+1].imag());
+                } else {
+                    ::new (&B[swap_row_offset + jdx*2]) cplx_select_t<small_scalar_type>(mtx[ row_offset + jdx*2].real(), mtx[ row_offset + jdx*2].imag());
+                    ::new (&B[swap_row_offset + jdx*2+1]) cplx_select_t<small_scalar_type>(mtx[ row_offset + jdx*2+1].real(), mtx[ row_offset + jdx*2+1].imag());
+                }
+                scale_factor_B += std::norm(B[swap_row_offset + jdx*2]);
+                scale_factor_B += std::norm(B[swap_row_offset + jdx*2+1]);
+            }
+        }
+#else
         // get the binary representation of permutation_idx
         // also get the number of 1's in the representation and their position as 2*i and 2*i+1 in consecutive slots of the vector bin_rep
         std::vector<unsigned char> bin_rep;
@@ -228,6 +467,9 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate(unsigned long long 
                 scale_factor_B = scale_factor_B + element.real()*element.real() + element.imag()*element.imag();
             }
         }
+        // fact corresponds to the (-1)^{(n/2) - |Z|} prefactor from Eq (3.24) in arXiv 1805.12498
+        bool fact = ((dim_over_2 - number_of_ones/2) % 2);
+#endif
 
 
         // scale matrix B -- when matrix elements of B are scaled, larger part of the computations can be kept in double precision
@@ -244,17 +486,18 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate(unsigned long long 
         // calculating Tr(B^j) for all j's that are 1<=j<=dim/2
         // this is needed to calculate f_G(Z) defined in Eq. (3.17b) of arXiv 1805.12498
         matrix_type traces(dim_over_2, 1);
+#ifndef GLYNN
         if (number_of_ones != 0) {
+#endif
             CalcPowerTraces<small_scalar_type, scalar_type>(B, dim_over_2, traces);
+#ifndef GLYNN
         }
         else{
             // in case we have no 1's in the binary representation of permutation_idx we get zeros
             // this occurs once during the calculations
             memset( traces.get_data(), 0.0, traces.rows*traces.cols*sizeof(complex_type));
         }
-
-        // fact corresponds to the (-1)^{(n/2) - |Z|} prefactor from Eq (3.24) in arXiv 1805.12498
-        bool fact = ((dim_over_2 - number_of_ones/2) % 2);
+#endif
 
 
         // auxiliary data arrays to evaluate the second part of Eqs (3.24) and (3.21) in arXiv 1805.12498
@@ -291,7 +534,7 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate(unsigned long long 
             memcpy(p_aux1, p_aux0, (dim_over_2+1)*sizeof(complex_type) );
 
             for (size_t jdx = 1; jdx <= (dim / (2 * idx)); jdx++) {
-                powfactor = powfactor * factor / ((small_scalar_type)jdx);
+                powfactor = powfactor * factor / ((scalar_type)jdx);
 
                 for (size_t kdx = idx * jdx + 1; kdx <= dim_over_2 + 1; kdx++) {
                     p_aux1[kdx-1] += p_aux0[kdx-idx*jdx - 1]*powfactor;
@@ -338,7 +581,9 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::calculate(unsigned long long 
 
     // scale the result by the appropriate factor according to Eq (2.11) of in arXiv 1805.12498
     res = res * pow(scale_factor, dim_over_2);
-
+#ifdef GLYNN
+    res /= (1ULL << (dim_over_2-1));
+#endif
     return Complex16(res.real(), res.imag() );
 }
 
@@ -357,6 +602,13 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::Update_mtx( matrix &mtx_in) {
     ScaleMatrix();
 
 
+}
+
+template <>
+void
+PowerTraceHafnian<RationalInf, RationalInf>::ScaleMatrix() {
+    mtx = mtx_orig;
+    scale_factor = 1.0;
 }
 
 
@@ -395,6 +647,9 @@ PowerTraceHafnian<small_scalar_type, scalar_type>::ScaleMatrix() {
 
 }
 
+template class PowerTraceHafnian<double, double>;
 template class PowerTraceHafnian<double, long double>;
+template class PowerTraceHafnian<long double, long double>;
+template class PowerTraceHafnian<RationalInf, RationalInf>;
 
 } // PIC
