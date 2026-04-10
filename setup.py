0ef31d90c0cfe07c5f2ce86fb848fd4af21b7d59
Gregory Morse <gregory.morse@live.com>
2026-04-10 14:56:47 +0200

    chore: add Python 3.10–3.13 classifiers for PyPI badge support

--- setup.py
+++ setup.py
@@ -45,6 +45,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Programming Language :: C",
         "Programming Language :: C++"
     ],
