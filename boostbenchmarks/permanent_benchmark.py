34629ac57819aa7ef732cc4b0ce3589fa684d86f
Gregory Morse <gregory.morse@live.com>
2026-04-14 09:06:30 +0200

    fix: update copyright years to 2026, add missing headers, fix tests, add tests/ to CI
    - Update all source files from Copyright 2021-2022 (and other old years)
      to Copyright 2021-2026
    - Add Apache 2.0 copyright headers to files missing them entirely
      (boostbenchmarks/boson_sampling_benchmark.py, cmake/find_numpy_blas_dir.py,
       ctests/permanents_accuracy.cpp, ctests/permanents_speed.cpp,
       piquassoboost/sampling/GlynnPermanentCalculator_Wrapper.hpp,
       piquassoboost/sampling/source/Batched/Glynn*DFE.cpp)
    - Normalize C++ license comment blocks from /** to /* to match expected format
    - Add piquasso-module, _skbuild, piquassoboost.egg-info to copyright test
      exceptions; handle shebang lines in copyright window calculation
    - Fix test_counts_raises: PureFockSimulator+HomodyneMeasurement now raises
      InvalidState during execution in piquasso v7+; catch gracefully
    - Add tests/ suite step to build-and-test-linux CI job (Linux only);
      install required extra deps (pytest-lazy-fixtures, jax[cpu], matplotlib, qiskit)
    - Increase Linux CI job timeout to 40 minutes

--- boostbenchmarks/permanent_benchmark.py
+++ boostbenchmarks/permanent_benchmark.py
@@ -1,11 +1,5 @@
-from piquasso._math.permanent import permanent
-
-from piquassoboost.connector import cpp_permanent_function
-
-
-
 #
-# Copyright 2021-2024 Budapest Quantum Computing Group
+# Copyright 2021-2026 Budapest Quantum Computing Group
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,6 +12,10 @@ from piquassoboost.connector import cpp_permanent_function
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from piquasso._math.permanent import permanent
+
+from piquassoboost.connector import cpp_permanent_function
 
 """
 Benchmarking the Piquasso vs. TheWalrus hafnian with repetitions.
